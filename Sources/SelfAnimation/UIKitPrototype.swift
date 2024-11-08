b2c6bae0003b0b354dda126b8dcdefa23b9e83fa
msventurini <42521920+msventurini@users.noreply.github.com>
2024-11-08 10:52:37 -0300

    initial commit

--- Sources/SelfAnimation/UIKitPrototype.swift
+++ Sources/SelfAnimation/UIKitPrototype.swift
@@ -8,9 +8,17 @@
 import UIKit
 import SwiftUI
 
-class CALayerTesting: UIViewController {
+public class CALayerTesting: UIViewController {
     
-    override func viewDidLoad() {
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func viewDidLoad() {
 
         super.viewDidLoad()
         
