4ee98230778d9c7535565ac742e20264a650dfe0
msventurini <42521920+msventurini@users.noreply.github.com>
2024-11-27 02:31:02 -0300

    backup das referências

---
+++ TestingShapes/TestingShapes/TestingShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingShapesApp.swift
+//  TestingShapes
+//
+//  Created by Matheus Silveira Venturini on 27/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
