893a5938452344e55ae9aac534f51aa3eb607468
msventurini <42521920+msventurini@users.noreply.github.com>
2024-11-28 00:29:44 -0300

    backup dos assets

--- Sources/SelfAnimation/Prod/SelfShapeHomeView.swift
+++ Sources/SelfAnimation/Prod/SelfShapeHomeView.swift
@@ -75,32 +75,7 @@ public class SelfShapeHomeView: UIView {
     
 }
 
-public struct ShapeHomeRepresentable: UIViewRepresentable {
-    
-    let shape: SelfShape
-    let weight: ShapeWeight
-    
-    public init(shape: SelfShape, weight: ShapeWeight) {
-        self.shape = shape
-        self.weight = weight
-    }
-    
-    public func makeUIView(context: Context) -> SelfShapeHomeView {
-        let view = SelfShapeHomeView(selfShape: shape, weight: weight)
-        return view
-    }
-    
-    public func updateUIView(_ uiView: SelfShapeHomeView, context: Context) {
-        
-    }
-    
-    public typealias UIViewType = SelfShapeHomeView
-    
-    public func sizeThatFits(_ proposal: ProposedViewSize, uiView: SelfShapeHomeView, context: Context) -> CGSize? {
-        return uiView.intrinsicContentSize
-    }
-    
-}
+
 
 #Preview {
     
